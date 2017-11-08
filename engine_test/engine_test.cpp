bf19823b82ae8839c323ee89ee046cb52438326e
areistel <alex.reistel.gmail.com>
2017-11-08 13:28:51 +0100

    klassenerstellung

--- engine_test/engine_test.cpp
+++ engine_test/engine_test.cpp
@@ -6,8 +6,17 @@
 #include "Gosu/Autolink.hpp"
 using namespace std;
 
-const int Window_size_x = 640;
-const int Window_size_y = 480;
+const int Window_size_x = 1000;
+const int Window_size_y = 800;
+
+
+class Motorrad  {
+
+public:
+	double x_taste = 500;
+	double x_moto;
+
+};
 
 class GameWindow : public Gosu::Window {
 public:
@@ -17,82 +26,66 @@ public:
 		: Window(Window_size_x, Window_size_y)
 		, bild("scrambler_021-01.png")
 	{
-		set_caption("Gosu Tutorial Game");
+
+		set_caption("Nices Game");
 	}
+	void update() override;
+	void draw() override;
 	
-	double x_taste =320;
-	double x_mouse;		//unnötig?
-	double y_mouse;
-	double x_moto;
-	double y;
-	bool mlinks = 0;
 	
-
-	void update() override {
-
-		if (input().down(Gosu::KB_LEFT) ==1)
-		{
-			x_taste = x_taste - 5;
-		}
-		if (input().down(Gosu::KB_RIGHT) == 1)
-		{
-			x_taste = x_taste +	5;
-		}
-		x_mouse = input().mouse_x();
-		y_mouse = input().mouse_y();
-
-		mlinks = input().down(Gosu::MS_LEFT);
-
-		/*x = (x + 1) % 300;
-		y = (y + 1) % 300;*/
-
-	}
-	void draw() override {
-		if (mlinks == true)
-		{
-			Gosu::Graphics::draw_triangle(
-				x_mouse, y_mouse, Gosu::Color::RED,
-				200 - x_mouse, 100 + y_mouse, Gosu::Color::GREEN,
-				200 + x_mouse, 400 - y_mouse, Gosu::Color::BLUE,
-				0.0
-			);
-		}
-
-
-		if (x_taste <= 615 && x_taste >= 25)
-		{
-			x_moto = x_taste;
-		}
-		
-		if (x_taste < 615 && x_taste < 25)
-		{
-			x_moto = 25;
-			x_taste = 25;
-		}
-		
-		if (x_taste > 615 && x_taste > 25)
-		{
-			x_moto = 615;
-			x_taste = 615;
-		}
-
-		bild.draw_rot(
-			x_moto, 430,
-			0,
-			0,
-			0.5,
-			0.5,
-			0.1,0.1		//scale
-			
-		);
-		
-		//das is n test
-
-		
-	}
+	Motorrad m1;	
 
 	
 };
+
+
+
+
+
+void GameWindow::update() {
+
+
+	if (m1.x_taste <= 960 && m1.x_taste >= 40)		//x_moto aktualisieren
+	{
+		m1.x_moto = m1.x_taste;
+	}
+
+	if (m1.x_taste < 960 && m1.x_taste < 40)		//linksseitige sperre
+	{
+		m1.x_moto = 40;
+		m1.x_taste = 40;
+	}
+
+	if (m1.x_taste > 960 && m1.x_taste > 40)		//rechtsseitige sperre
+	{
+		m1.x_moto = 960;
+		m1.x_taste = 960;
+	}
+
+	if (input().down(Gosu::KB_LEFT) == 1)			//input linke pfeiltaste
+	{
+		m1.x_taste = m1.x_taste - 5;
+	}
+	if (input().down(Gosu::KB_RIGHT) == 1)			//input rechte pfeiltaste
+	{
+		m1.x_taste = m1.x_taste + 5;
+	}
+}
+
+void GameWindow::draw() {
+
+
+	bild.draw_rot(									//bild erstellen mit entsprechenden koordinaten
+		m1.x_moto, Window_size_y-90,
+		0,
+		0,
+		0.5,
+		0.5,
+		0.15, 0.15
+
+	);
+
+}
 
 
 int main() {
